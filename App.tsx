980f71ba29967f10c35250863fc1169017af351f
andrei-kashkan <andrei.kashkan@alphapoint.com>
2022-08-25 22:29:49 +0400

    Added setting

--- App.tsx
+++ App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import {SafeAreaView, View} from 'react-native';
-import {ApplicationProvider, Layout, Text, Button} from '@ui-kitten/components';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {
+  ApplicationProvider,
+  Layout,
+  Text,
+  Button,
+  IconRegistry,
+} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {default as mapping} from './mapping.json';
 
 const App: React.FC = () => {
   return (
-    <ApplicationProvider
-      {...eva}
-      theme={eva.dark}
-      customMapping={{...eva.mapping, ...mapping}}>
-      <Layout>
-        <SafeAreaView>
-          <View>
-            <Text category="h1">bulbasaur</Text>
-            <Button size={'small'}>Click me</Button>
-          </View>
-        </SafeAreaView>
-      </Layout>
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider
+        {...eva}
+        theme={eva.light}
+        customMapping={{...eva.mapping, ...mapping}}>
+        <Layout style={styles.layout}>
+          <SafeAreaView>
+            <View>
+              <Text category="h1">bulbasaur</Text>
+              <Button size={'small'}>Click me</Button>
+            </View>
+          </SafeAreaView>
+        </Layout>
+      </ApplicationProvider>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+});
 
 export default App;
